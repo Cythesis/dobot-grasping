b83583d318636149c8c66cbf0b84f0456b68050b
Cythesis <52483870+Cythesis@users.noreply.github.com>
2021-05-01 15:55:31 +1000

    Corrected topics

--- lib/RosPublish.m
+++ lib/RosPublish.m
@@ -4,7 +4,7 @@ classdef RosPublish
     end
     methods
         function sendTraj(obj,pos)
-            channel_1 = rospublisher("/target_joint_trajectory","trajectory_msgs/JointTrajectory");
+            channel_1 = rospublisher("/target_joint_states","trajectory_msgs/JointTrajectory");
             msg = rosmessage(channel_1);
             point = rosmessage('trajectory_msgs/JointTrajectoryPoint');
             point.Positions = pos;
@@ -14,7 +14,7 @@ classdef RosPublish
         end
         
         function sendCart(obj,x,y,z,qw,qx,qy,qz)
-            channel_1 = rospublisher("/target_pose_trajectory","geometry_msgs/Pose");
+            channel_1 = rospublisher("/target_end_effector_pose","geometry_msgs/Pose");
             msg = rosmessage(channel_1);
             point = rosmessage('geometry_msgs/Point');
             quaternion = rosmessage('geometry_msgs/Quaternion');
